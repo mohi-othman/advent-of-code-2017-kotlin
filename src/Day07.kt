41ada2b3002becaab3560e9e8e3b505552fca5b4
mohi-othman <mohyiddeen@yahoo.com>
2017-12-08 12:11:04 -0800

    Day 7 - Part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,15 +1,65 @@
 import java.io.File
 import java.io.InputStream
+import kotlin.math.sign
 
-class TreeNode(name: String, weight: Int, children: List<String>?, parent: TreeNode? = null) {
+class TreeNode(name: String, weight: Int, children: List<String>?) {
     val Name = name
     val Weight = weight
     val Children = children
-    var Parent = parent
+    var Parent: TreeNode? = null
+    var CalcWeight = 0
+}
+
+fun List<TreeNode>.FindRoot(): TreeNode {
+    return this.filter { node -> node.Parent == null }.first()
+}
+
+fun List<TreeNode>.CalcWeights() {
+    this.CalcWeights(this.FindRoot())
+}
+
+fun List<TreeNode>.CalcWeights(node: TreeNode) {
+    node.CalcWeight = node.Weight
+    this.GetChildrenNodesOf(node)
+        .forEach { child ->
+                this.CalcWeights(child!!)
+                node.CalcWeight += child.CalcWeight
+        }
+}
+
+fun List<TreeNode>.FindImbalancedNodeNewWeight(): Int {
+    val imbalancedNode = this.FindImbalancedNode(this.FindRoot())
+    val siblings = this.GetChildrenNodesOf(imbalancedNode.Parent!!).filter { node -> node != imbalancedNode }
+    return imbalancedNode.Weight + (siblings.first().CalcWeight - imbalancedNode.CalcWeight)
+}
+
+fun List<TreeNode>.FindImbalancedNode(node: TreeNode): TreeNode {
+    var children = this.GetChildrenNodesOf(node)
+    var weights = children.map { child -> child.CalcWeight }.distinct()
+    if(weights.size > 1) {
+        val imbalancedWeight = weights.find { weight -> children.filter { child -> child.CalcWeight == weight }.size == 1 }
+        return this.FindImbalancedNode(children.find { child->child.CalcWeight == imbalancedWeight }!!)
+    }
+    else {
+        return node
+    }
+}
+
+fun List<TreeNode>.GetChildrenNodesOf(node: TreeNode): List<TreeNode> {
+    if (node.Children == null) return emptyList()
+    return node.Children
+            .map { name -> this.find { child -> child.Name == name } }
+            .filterNotNull()
+            .toList()
 }
 
 fun Solve07_1(nodes: List<TreeNode>): String {
-    return nodes.filter { node -> node.Parent == null }.first().Name
+    return nodes.FindRoot().Name
+}
+
+fun Solve07_2(nodes: List<TreeNode>): Int {
+    nodes.CalcWeights()
+    return nodes.FindImbalancedNodeNewWeight()
 }
 
 fun PopulateTree(input: String): List<TreeNode> {
@@ -43,5 +93,20 @@ fun LoadData07(): String {
 
 fun main(args: Array<String>) {
     val l = PopulateTree(LoadData07())
+
+     /*val l = PopulateTree("pbga (66)\n" +
+            "xhth (57)\n" +
+            "ebii (61)\n" +
+            "havc (66)\n" +
+            "ktlj (57)\n" +
+            "fwft (72) -> ktlj, cntj, xhth\n" +
+            "qoyq (66)\n" +
+            "padx (45) -> pbga, havc, qoyq\n" +
+            "tknk (41) -> ugml, padx, fwft\n" +
+            "jptl (61)\n" +
+            "ugml (68) -> gyxo, ebii, jptl\n" +
+            "gyxo (61)\n" +
+            "cntj (57)")*/
     println(Solve07_1(l))
+    println(Solve07_2(l))
 }
